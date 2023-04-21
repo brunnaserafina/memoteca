6712d6233dde98176f7dd10a147df742abb79de4
Brunna Serafina <brunnaserafina@gmail.com>
2023-04-21 20:41:43 -0300

    feat: listing thoughts and creating routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
+import { ListThoughtsComponent } from './components/thoughts/list-thoughts/list-thoughts.component';
+import { CreateThoughtsComponent } from './components/thoughts/create-thoughts/create-thoughts.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listThoughts',
+    pathMatch: 'full',
+  },
+  {
+    path: 'createThought',
+    component: CreateThoughtsComponent,
+  },
+  {
+    path: 'listThoughts',
+    component: ListThoughtsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CreateThoughtsComponent } from './components/thoughts/create-thoughts/create-thoughts.component';
 import { FormsModule } from '@angular/forms';
+import { ListThoughtsComponent } from './components/thoughts/list-thoughts/list-thoughts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CreateThoughtsComponent
+    CreateThoughtsComponent,
+    ListThoughtsComponent
   ],
   imports: [
     BrowserModule,
