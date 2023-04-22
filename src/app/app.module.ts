bf97d3c7ef3866d5acbe91fa20180b6c561afe06
Brunna Serafina <brunnaserafina@gmail.com>
2023-04-22 14:42:50 -0300

    style: centering main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { CreateThoughtsComponent } from './components/thoughts/create-thoughts/create-thoughts.component';
 import { FormsModule } from '@angular/forms';
 import { ListThoughtsComponent } from './components/thoughts/list-thoughts/list-thoughts.component';
+import { CardThoughtComponent } from './components/thoughts/card-thought/card-thought.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ListThoughtsComponent } from './components/thoughts/list-thoughts/list-
     HeaderComponent,
     FooterComponent,
     CreateThoughtsComponent,
-    ListThoughtsComponent
+    ListThoughtsComponent,
+    CardThoughtComponent
   ],
   imports: [
     BrowserModule,
