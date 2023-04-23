9d5f2331a192fa6fb1a25afd6a299a9eba5f3024
Brunna Serafina <brunnaserafina@gmail.com>
2023-04-23 18:02:57 -0300

    feat: adding route for editing thoughts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { EditThoughtComponent } from './components/thoughts/edit-thought/edit-thought.component';
 import { DeleteThoughtComponent } from './components/thoughts/delete-thought/delete-thought.component';
 import { ListThoughtsComponent } from './components/thoughts/list-thoughts/list-thoughts.component';
 import { CreateThoughtsComponent } from './components/thoughts/create-thoughts/create-thoughts.component';
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'thoughts/deleteThought/:id',
     component: DeleteThoughtComponent,
+  },
+  {
+    path: 'thoughts/editThought/:id',
+    component: EditThoughtComponent,
   },
 ];
 
---
+++ src/app/components/thoughts/edit-thought/edit-thought.component.ts
@@ -0,0 +1,41 @@
+import { Router, ActivatedRoute } from '@angular/router';
+import { ThoughtService } from './../thought.service';
+import { Pensamento } from './../thought';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-thought',
+  templateUrl: './edit-thought.component.html',
+  styleUrls: ['./edit-thought.component.css'],
+})
+export class EditThoughtComponent implements OnInit {
+  pensamento: Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: '',
+  };
+
+  constructor(
+    private service: ThoughtService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
+      this.pensamento = pensamento;
+    });
+  }
+
+  editarPensamento() {
+    this.service.editar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listThoughts']);
+    });
+  }
+
+  cancelarPensamento() {
+    this.router.navigate(['/listThoughts']);
+  }
+}
